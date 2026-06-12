e62dc75b4531a4b334031c77f3ee3cb55f879068
gbouras13 <george.bouras@adelaide.edu.au>
2026-06-12 13:02:09 +0930

    fasta_in cleanup

--- src/baktfold/io/fasta_in.py
+++ src/baktfold/io/fasta_in.py
@@ -1,12 +1,4 @@
-# import atexit
-# import json
-# import logging
-# import os
-# import sys
-# import binascii
-# import gzip
-# from typing import List, Tuple, TextIO
-# from datetime import datetime
+import sys
 from pathlib import Path
 
 from loguru import logger
@@ -25,7 +17,8 @@ def parse_protein_input(input_path, faa_path):
     returns cds_dict
     """
 
-    # handles regular FASTA and gzipped 
+    # handles regular FASTA and gzipped
+    fasta_flag = False
     try:
         if input_path == '':
             raise ValueError('File path argument must be non-empty')
@@ -35,20 +28,24 @@ def parse_protein_input(input_path, faa_path):
             logger.info('FASTA input format detected.')
         else:
             logger.info('Bakta JSON input format detected. Only hypothetical proteins from the Bakta JSON input file will be annotated.')
-    except:
-        logger.error(f'ERROR: annotation file {input_path} not valid!')
-    
+    except Exception as e:
+        logger.error(f'ERROR: annotation file {input_path} not valid! {e}')
+        sys.exit(1)
+
+    aas = []
+    hypotheticals = []
+    bakta_version = {}
     try:
         if fasta_flag:
             logger.info('Attempting to parse input protein sequences as .faa format ...')
             aas = fasta.import_sequences(input_path, False, False)
-            bakta_version = {}
         else:
             logger.info('Attempting to parse input protein sequences as Bakta JSON format ...')
             aas, hypotheticals, bakta_version = parse_json_input(input_path, False, False, protein_json_flag=True)
         logger.info(f'Imported sequences={len(aas)}')
-    except:
-        logger.error('ERROR: wrong file format or unallowed characters in amino acid sequences!')
+    except Exception as e:
+        logger.error(f'ERROR: wrong file format or unallowed characters in amino acid sequences! {e}')
+        sys.exit(1)
     
     mock_start = 1
     for aa in aas:  # rename and mock feature attributes to reuse existing functions
